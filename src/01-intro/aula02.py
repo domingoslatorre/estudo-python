2503b6279c67b969906ebc42cffff31ab4821b73
Domingos Latorre <domingos.latorre@gmail.com>
2023-05-01 16:03:40 -0300

    aula 02 - keywords e identificadores

---
+++ src/01-intro/aula02.py
@@ -0,0 +1,35 @@
+""" Aula 02 - Keywords e identificadores """
+
+# Palavras reservadas
+# True, False, None, and, import, lambda
+
+# Identificadores
+# nome de variáveis, funções, classes
+# case sensitive
+# devem iniciar com uma letra ou _
+# nao pode ter espaços em branco
+# nao pode usar caracteres especias !, @, #, $
+
+nome = 'Maria'
+idade = 30
+Nome = 'João'
+nome_completo = 'Maria da Silva'
+
+
+# Boas práticas (clean code)
+c = 10
+contador = 10
+
+s = 10 + 20
+soma = 10 + 20
+
+# Constantes
+PI = 3.14
+
+MAIORIDADE = 18
+idade = 18
+
+if idade >= MAIORIDADE:
+    print('Maior de idade')
+else:
+    print('Menor de idade')
