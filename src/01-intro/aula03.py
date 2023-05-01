dbf02b1ae3a011e589944cda787d6cdfab049815
Domingos Latorre <domingos.latorre@gmail.com>
2023-05-01 16:04:14 -0300

    aula 03 - comentários

---
+++ src/01-intro/aula03.py
@@ -0,0 +1,18 @@
+""" Aula 03 - Comentários """
+
+# Comentário de única linha
+
+'''
+Isso é um comentário
+de múltiplas 
+linhas
+'''
+
+# atribui o valor 10 a variável n1
+n1 = 10
+
+n2 = 30
+n3 = 78
+
+print(n1, n2)
+print(n3)
