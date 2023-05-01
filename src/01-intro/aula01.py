95e24067483a661d2b2e3520ed54ffbbb8cb8476
Domingos Latorre <domingos.latorre@gmail.com>
2023-05-01 15:36:01 -0300

    commit inicial

---
+++ src/01-intro/aula01.py
@@ -0,0 +1,14 @@
+""" aula01 """
+
+print('olá mundo!!!')
+
+
+def somar(numero_01, numero_02):
+    """ somar - soma dois numeros """
+    return numero_01 + numero_02
+
+
+soma = somar(10.3, 2.5)
+
+
+print(soma)
